8d044846a79a6b26e97197e232519e060bd2c1bd
Enochen <sirenochen@gmail.com>
2024-08-14 22:17:59 -0400

    feat: add generated client

---
+++ generate.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+set -e
+
+URL="https://api.artifactsmmo.com/openapi.json"
+
+echo "Downloading specs..."
+curl -L "$URL" -o openapi.json
+
+echo "Generating client..."
+openapi-generator-cli generate -i openapi.json -g rust --additional-properties=useSingleRequestParameter=true,preferUnsignedInt=true
+
+echo "Fixing generated code..."
+cargo clippy --fix --allow-dirty
+cargo fmt
+
+echo "Done!"
